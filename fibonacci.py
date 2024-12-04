fe7d1cc562e24d8eb3f67e4201d86466aeea2675
amaan@gmail.com <amaan@gmail.com>
2024-12-04 15:21:21 +0530

    Done the correction and Resubmitting it

--- fibonacci.py
+++ fibonacci.py
@@ -1,17 +1,22 @@
-#Assignment-2
-#Task-1
 def fib(n):
+    if n <= 0:
+        print("Input must be a positive integer.") #if n is negative or 0
+        return
+    
     a = 0
-    b = 1
-    if n==1:
-        print(a)
-    else:
+    b = 1 
+    
+    if n == 1:
+        print(a)  #if n is 1
+        return
+    else: 
         print(a)
         print(b)
-        for i in range(2,n):
+        
+        for i in range(2, n):
             c = a + b
             a = b
             b = c
             print(c)
-fib(10)                    
-    
+
+fib(5)  
